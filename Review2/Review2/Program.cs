d1587dc7d15b340fed0470519dfc3ce372e1ae78
srisri96 <srikanthsitharth80@gmil.com>
2022-07-16 19:04:48 +0530

    Employee present

---
+++ Review2/Review2/Program.cs
@@ -0,0 +1,5 @@
+﻿using Review;
+//caling by direct method name
+UC1wage.Empsal(); 
+//UC1wage obje = new UC1wage(); calling by object creation
+//obje.Empsal();
---
+++ Review2/Review2/UC1wage.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Review
+{
+    class UC1wage
+    {
+        public static void Empsal()
+        {
+            //constant declaration
+            int Full_Time = 1;
+            Random random = new Random();
+            //computation
+            int empcheck = random.Next(0, 2);
+            if (empcheck == Full_Time)
+            {
+                Console.WriteLine("Employee is present");
+            }
+            else
+            {
+                Console.WriteLine("Emloyee is absent");
+            }
+        }
+    }
+}
